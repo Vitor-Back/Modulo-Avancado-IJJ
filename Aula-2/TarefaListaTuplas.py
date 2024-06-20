6f9d08c4df45fd74813a81bb10cb167d4f9cbaa0
Vitor Back <vitor.back10@outlook.com>
2024-06-20 16:21:22 -0400

    Pastas dos exercicios feitos no modulo avançad até dia 20/06/02024

---
+++ Aula-2/TarefaListaTuplas.py
@@ -0,0 +1,20 @@
+campeoesWildRift = ("Urgot - Top", "Amummu - JG", "Zigs - Mid", "Leona - Sup", "Jhin - ADC")
+
+listaCampeao = list(campeoesWildRift)
+
+print(listaCampeao)
+
+listaCampeao.extend(["Veigar - Mid", "Teemo - Top"])
+
+print(listaCampeao)
+
+listaCampeao.pop()
+
+print(listaCampeao)
+
+print(listaCampeao[0])
+
+totalElementos = len(listaCampeao)
+
+print(totalElementos)
+
