3ee1ae4d2598acdf738c58195fbd564e97fd0e0e
Vitor Back <vitor.back10@outlook.com>
2024-06-28 20:20:01 -0400

    Atualizações aula passada - Exercicios Estrutura de repetição - Tarefa Consulta cidade

---
+++ Aula-5/Inportandolib.py
@@ -0,0 +1,9 @@
+import requests
+
+cep = input('Informe um CEP: ') 
+response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+
+data = response.json()
+
+print(f':\n', 'Localidade:', data['localidade'], data['cep'], data['logradouro'])
+
