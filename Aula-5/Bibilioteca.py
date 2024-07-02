4c5260dfe92ee95b9ec0e409edceabaf12d63f22
Vitor Back <vitor.back10@outlook.com>
2024-07-02 18:26:22 -0400

    -Arquivo.gitgnore -Funções

---
+++ Aula-5/Bibilioteca.py
@@ -0,0 +1,8 @@
+import os
+
+dados = ["DarkSouls III", "Lies Of Pi", "Elden Ring", "Elden Ring - Shadow of the Erdtree"]
+
+#Variavel com nome do diretório 
+diretorio = 'Desafio_OS'
+
+# Comando para criar o diretório (O parâmetro 'exist_ok=True' impede que ocorra um erro se o diretório já existir.)
